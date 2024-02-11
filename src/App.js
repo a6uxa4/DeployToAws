73f0e1ffc73ff9e66aed1b8db3da8264805abcd6
Abu <104144116+a6uxa4@users.noreply.github.com>
2024-02-12 00:30:31 +0600

    fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Buttons1 } from "./buttons/part-1";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Deploy to AWS through amplify</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Buttons1 />
       </header>
     </div>
   );
---
+++ src/buttons/part-1/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./index.css";
+
+export const Buttons1 = () => {
+  return (
+    <div className="container">
+      <a href="/" tabIndex="0">
+        <img
+          class="bn45"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/2560px-Google_Play_Store_badge_EN.svg.png"
+          alt="bn45"
+        />
+      </a>
+
+      <a href="/" tabIndex="0">
+        <img
+          class="bn46"
+          src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
+          alt="bn45"
+        />
+      </a>
+    </div>
+  );
+};
